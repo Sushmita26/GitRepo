d26d5c6bd76b4c85c3f970e2425eb36c6524f363
sushmita26 <sushmita.kumari@wipro.com>
2017-04-17 18:24:33 +0530

    Commit

---
+++ MVC/WebContent/WEB-INF/EmployeeForm.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+<style>
+
+</style>
+</head>
+<body>
+<h1>Employee Form</h1>
+<form action="/MVC/submitEmployeeForm.html" method="post">
+<p>Employee Name:<input type="text" name="employeeName"/>
+</p>
+
+<p>Employee Age:<input type="text" name="employeeAge"/>
+</p>
+<input type="submit" value="submit this form by clicking here"/>
+</form>
+</body>
+</html>
---
+++ MVC/src/com/mvccontroller/MVC_Controller.java
@@ -0,0 +1,31 @@
+package com.mvccontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MVC_Controller {
+	
+	@RequestMapping(value="/employeeForm.html" , method = RequestMethod.GET)
+	public ModelAndView getEmployeeForm()
+	{
+		ModelAndView model = new ModelAndView("EmployeeForm");
+		
+		return model;
+	
+
+	}
+	
+	@RequestMapping(value="/submitEmployeeForm.html" , method = RequestMethod.POST)
+	public ModelAndView submitEmployeeForm(@RequestParam("employeeName") String name , @RequestParam("employeeAge") int num)
+	{
+		ModelAndView model = new ModelAndView("EmployeeSuccess");
+		model.addObject("msg","Details submitted by you:: Name: "+name+ " , Age:" + num);
+		
+		return model;
+	}
+
+}
